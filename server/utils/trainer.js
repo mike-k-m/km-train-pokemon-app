b60daf6689848003ea3c8d7a8c60fd4e313077b4
mike <mike.mok@konicaminolta.com>
2023-03-08 11:02:24 +0900

    スケルトンままのコミット

---
+++ server/utils/trainer.js
@@ -0,0 +1,38 @@
+import { ListObjectsCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import s3Client from "./s3Client";
+
+const config = useRuntimeConfig();
+
+const streamToString = (stream) =>
+  new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+  });
+
+/** トレーナーの一覧の取得 */
+export const findTrainers = async () => {
+  const objects = await s3Client.send(
+    new ListObjectsCommand({ Bucket: config.bucketName })
+  );
+  return objects.Contents ?? [];
+};
+
+/** トレーナーの取得 */
+// TODO: トレーナーを取得する S3 クライアント処理の実装
+
+/** トレーナーの追加更新 */
+export const upsertTrainer = async (name, trainer) => {
+  const result = await s3Client.send(
+    new PutObjectCommand({
+      Bucket: config.bucketName,
+      Key: `${name}.json`,
+      Body: JSON.stringify({ name: "", pokemons: [], ...trainer }),
+    })
+  );
+  return result;
+};
+
+/** トレーナーの削除 */
+// TODO: トレーナーを削除する S3 クライアント処理の実装
