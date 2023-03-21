206db8567810ad07f3f2c55c71e4b1cf2b4290be
mike <mike.mok@konicaminolta.com>
2023-03-21 18:46:31 +0900

    フロントエンド実装：ポケモンをつかまえる画面、ポケモン一覧をページ毎にリスト更新対応

--- pages/trainerInfo/[name]/catchPokemon.vue
+++ pages/trainerInfo/[name]/catchPokemon.vue
@@ -4,10 +4,47 @@ console.log(`cathPokemon Enter`);
 const route = useRoute();
 const trainerName = route.params.name;
 
-// const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/`);
-const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9999`);
-const pokemonList = data.value.results;
-const pokemonCount = data.value.count;
+// PokeAPIのURL、現在のURL、前のURL(previousプロパティ)、次のURL(nextプロパティ)
+const currentURL = ref("https://pokeapi.co/api/v2/pokemon/"); // 初期値は https://pokeapi.co/api/v2/pokemon/
+const nextURL = ref("");
+const previousURL = ref("");
+
+const pokemonList = ref([]);  // Pokemonリストは、pokeAPI戻り値の"results"プロパティ。配列
+const pokemonCount = ref(0);
+
+// この画面に初回ロード時にPokeAPIをfetchする
+// const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9999`);
+const { data: pokeResult, refresh } = await useFetch(currentURL);
+
+// updateValues関数は、pokeAPIのfetchした後、pokeAPI戻り値から各変数に代入する
+const updateValues = () => {
+  pokemonList.value = pokeResult.value.results;
+  pokemonCount.value = pokeResult.value.count;
+  nextURL.value = pokeResult.value.next;
+  previousURL.value = pokeResult.value.previous;
+}
+
+updateValues();
+
+console.log(`next: ${nextURL.value}`);
+console.log(`previous: ${previousURL.value}`);
+
+// 前へのボタンを押せるかを制御するフラグ。previousURLが
+const canGoNext = computed( () => {
+  if (nextURL.value != "" || nextURL.value != null) {
+    return true;
+  } else {
+    return false;
+  }
+});
+const canGoPrevious = computed( () => {
+  if (previousURL.value != "" || nextURL.value != null) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
 // console.log(pokemonList);
 
 const onCatch = async (pokemonName) => {
@@ -27,16 +64,62 @@ const onCatch = async (pokemonName) => {
   }
 };
 
+const goNext = async () => {
+  console.log(`goNext() enter`);
+  console.log(`goNext() currentURL.value ${currentURL.value}`);
+  console.log(`goNext() pokeResult.value.next ${pokeResult.value.next}`);
+  console.log(`goNext() pokeResult.value.previous ${pokeResult.value.previous}`);
+  console.log(`goNext() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
+
+  currentURL.value = nextURL.value;
+
+  await refresh();
+  updateValues();
+  console.log(`goNext() ---------------------------------------------------`);
+  console.log(`goNext() After refresh()`);
+  console.log(`goNext() currentURL.value ${currentURL.value}`);
+  console.log(`goNext() pokeResult.value.next ${pokeResult.value.next}`);
+  console.log(`goNext() pokeResult.value.previous ${pokeResult.value.previous}`);
+  console.log(`goNext() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
+}
+
+const goPrevious = async () => {
+  console.log(`goNext() enter`);
+  console.log(`goNext() currentURL.value ${currentURL.value}`);
+  console.log(`goNext() pokeResult.value.next ${pokeResult.value.next}`);
+  console.log(`goNext() pokeResult.value.previous ${pokeResult.value.previous}`);
+  console.log(`goNext() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
+
+  currentURL.value = previousURL.value;
+
+  await refresh();
+  updateValues();
+  console.log(`goNext() ---------------------------------------------------`);
+  console.log(`goNext() After refresh()`);
+  console.log(`goNext() currentURL.value ${currentURL.value}`);
+  console.log(`goNext() pokeResult.value.next ${pokeResult.value.next}`);
+  console.log(`goNext() pokeResult.value.previous ${pokeResult.value.previous}`);
+  console.log(`goNext() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
+}
+
 </script>
 
 <template>
   <div>
     <h1>ポケモンをつかまえる</h1>
     <p>{{ pokemonCount }}種類のポケモン</p>
+    <div>
+      <GamifyButton :disabled="!canGoPrevious" @click="goPrevious()">前へ</GamifyButton>
+      <GamifyButton :disabled="!canGoNext" @click="goNext()">次へ</GamifyButton>
+    </div>
     <GamifyList v-for="(pokemon, index) in pokemonList" :key="index">
       <span>{{ pokemon.name }}　</span>
       <GamifyButton @click="onCatch(pokemon.name)">捕まえる</GamifyButton>
     </GamifyList>
+    <div>
+      <GamifyButton>前へ</GamifyButton>
+      <GamifyButton>次へ</GamifyButton>
+    </div>
   </div>
 </template>
 
