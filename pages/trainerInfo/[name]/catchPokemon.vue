dc7b4cdc13215cc0492a03c81b4530535eba929a
mike <mike.mok@konicaminolta.com>
2023-03-22 19:11:40 +0900

    フロントエンド実装：「ポケモンをつかまえる」画面のポケモン種類の数の実装

--- pages/trainerInfo/[name]/catchPokemon.vue
+++ pages/trainerInfo/[name]/catchPokemon.vue
@@ -10,10 +10,21 @@ const nextURL = ref("");
 const previousURL = ref("");
 
 const pokemonList = ref([]);  // Pokemonリストは、pokeAPI戻り値の"results"プロパティ。配列
-const pokemonCount = ref(0);
+const pokemonCount = ref(0);  // Pokemonカウントは、pokeAPI戻り値の"count"プロパティ。不変の用で、reactiveが不要かも
+
+// 現在のページに表示しているポケモンの数
+const currentPageCount = ref(0); 
+
+// 表示済みのpokemonの種類の数。
+const countAlreadyShown = ref(0);
+
+// 未表示のpokemonの種類
+const countYetToShow = computed( () => {
+  return pokemonCount.value - countAlreadyShown.value;
+});
 
 // この画面に初回ロード時にPokeAPIをfetchする
-// const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9999`);
+// const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9999`); //最初の実装PokeAPIを試すため
 const { data: pokeResult, refresh } = await useFetch(currentURL);
 
 // updateValues関数は、pokeAPIのfetchした後、pokeAPI戻り値から各変数に代入する
@@ -22,13 +33,13 @@ const updateValues = () => {
   pokemonCount.value = pokeResult.value.count;
   nextURL.value = pokeResult.value.next;
   previousURL.value = pokeResult.value.previous;
+  currentPageCount.value = pokeResult.value.results.length;
 }
 
 updateValues();
 
-console.log(`初回 currentURL -> ${currentURL.value}`);
-console.log(`初回 previousURL -> ${previousURL.value}`);
-console.log(`初回 nextURL -> ${nextURL.value}`);
+// 表示済みのpokemonの種類数は、記憶しなければならない。初回だけここで表示中に値をセットする。
+countAlreadyShown.value = currentPageCount.value;
 
 // 「前へ」、「次へ」ボタンでfetchし直すので、fetchしたデータの変化をwatchし、表示データを更新する。
 watch(pokeResult, () => {
@@ -57,56 +68,42 @@ const disableGoPrevious = computed( () => {
 const onCatch = async (pokemonName) => {
   console.log(`cathPokemon onCatch pokemonName: ${pokemonName}`);
   try {
-    const {data} = await useFetch(`/api/trainer/${trainerName}/pokemon/${pokemonName}`,
-                                  { 
-                                    method: 'put',
-                                    body: JSON.stringify({
-                                      trainerName: `${trainerName}`,
-                                      pokemonName: `${pokemonName}`
-                                    })
-                                  });
+    await useFetch(`/api/trainer/${trainerName}/pokemon/${pokemonName}`,
+                  { 
+                    method: 'put',
+                    body: JSON.stringify({
+                      trainerName: `${trainerName}`,
+                      pokemonName: `${pokemonName}`
+                    })
+                  });
     await navigateTo(`/trainerInfo/${trainerName}`);
   } catch(err) {
     console.log(`catchPokemon 追加エラー`, err);
   }
 };
 
-// 「次へ」ボタンのイベントハンドラー、currentURLを前回に取得したnextURLを代入し、再度fetchする。
+// 「次へ」ボタンのイベントハンドラー
 const goNext = async () => {
-  // console.log(`goNext() enter`);
-  // console.log(`goNext() currentURL.value ${currentURL.value}`);
-  // console.log(`goNext() pokeResult.value.next ${pokeResult.value.next}`);
-  // console.log(`goNext() pokeResult.value.previous ${pokeResult.value.previous}`);
-  // console.log(`goNext() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
-
+  // 次のページを遷移する前に、fetchするURL(currentURL)をnextURLに設定する。
   currentURL.value = nextURL.value;
 
+  // fetchする
   await refresh();
-  // console.log(`goNext() ---------------------------------------------------`);
-  // console.log(`goNext() After refresh()`);
-  console.log(`goNext() currentURL.value ${currentURL.value}`);
-  // console.log(`goNext() pokeResult.value.next ${pokeResult.value.next}`);
-  // console.log(`goNext() pokeResult.value.previous ${pokeResult.value.previous}`);
-  // console.log(`goNext() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
+
+  // 次のページに遷移したので、表示済みの種類数を今回Fetchした数を足す。
+  countAlreadyShown.value += currentPageCount.value;
 }
 
-// 「前へ」ボタンのイベントハンドラー、currentURLを前回に取得したnextURLを代入し、再度fetchする。
+// 「前へ」ボタンのイベントハンドラー
 const goPrevious = async () => {
-  // console.log(`goPrevious() enter`);
-  // console.log(`goPrevious() currentURL.value ${currentURL.value}`);
-  // console.log(`goPrevious() pokeResult.value.next ${pokeResult.value.next}`);
-  // console.log(`goPrevious() pokeResult.value.previous ${pokeResult.value.previous}`);
-  // console.log(`goPrevious() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
-
+  // 前のページを遷移する前に、fetchするURL(currentURL)をpreviousURLに設定する。
   currentURL.value = previousURL.value;
 
+  // 前のページ戻る前に、表示済の種類数を、現在の画面に表示中の種類数を減らす。
+  countAlreadyShown.value -= currentPageCount.value;
+
+  // fetchする
   await refresh();
-  // console.log(`goPrevious() ---------------------------------------------------`);
-  // console.log(`goPrevious() After refresh()`);
-  console.log(`goPrevious() currentURL.value ${currentURL.value}`);
-  // console.log(`goPrevious() pokeResult.value.next ${pokeResult.value.next}`);
-  // console.log(`goPrevious() pokeResult.value.previous ${pokeResult.value.previous}`);
-  // console.log(`goPrevious() pokeResult.value.results[0].name ${pokeResult.value.results[0].name}`);
 }
 
 </script>
@@ -114,14 +111,15 @@ const goPrevious = async () => {
 <template>
   <div>
     <h1>ポケモンをつかまえる</h1>
-    <p>{{ pokemonCount }}種類のポケモン</p>
+    <p>{{ pokemonCount }}種類のポケモン (表示済み：{{ countAlreadyShown }}種類, 未表示：{{ countYetToShow }}種類)</p>
+    <p>このページに表示中：{{currentPageCount}}種類</p>
     <GamifyButton @click="navigateTo('/')">はじめの画面へ</GamifyButton>
     <div>
       <GamifyButton :disabled="disableGoPrevious" @click="goPrevious()">前へ</GamifyButton>
       <GamifyButton :disabled="disableGoNext" @click="goNext()">次へ</GamifyButton>
     </div>
     <GamifyList v-for="(pokemon, index) in pokemonList" :key="index">
-      <span>{{ pokemon.name }}　</span>
+      <span style="margin-right: 200px;">{{ pokemon.name }}</span>
       <GamifyButton @click="onCatch(pokemon.name)">捕まえる</GamifyButton>
     </GamifyList>
     <div>
